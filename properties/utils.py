76beeb7fa9f900535292f249da16ce2579c9c608
Adedolapo Bakare <adedolapobakare16@gmail.com>
2025-08-30 15:59:40 +0100

    task 3

---
+++ properties/utils.py
@@ -0,0 +1,18 @@
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    # Try to fetch from cache
+    properties = cache.get("all_properties")
+
+    if properties is None:
+        # Not in cache → fetch from DB
+        properties = list(
+            Property.objects.all().values(
+                "id", "title", "description", "price", "location", "created_at"
+            )
+        )
+        # Store in cache for 1 hour (3600 seconds)
+        cache.set("all_properties", properties, timeout=3600)
+
+    return properties
--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
+from django.http import JsonResponse
+from .utils import get_all_properties
 
 # Cache for 15 minutes
 @cache_page(60 * 15)
@@ -10,4 +12,10 @@ def property_list(request):
     )
     return JsonResponse({
         "data": list(properties)
+    })
+
+def property_list(request):
+    properties = get_all_properties()
+    return JsonResponse({
+        "data": properties
     })
